f9c20069ecff6222e9d86391b00176efa7393217
Dana Contreras <70418201+DanaContreras@users.noreply.github.com>
2024-10-15 11:47:10 -0300

    feat: ej2a

--- tp3/delete.sql
+++ tp3/delete.sql
@@ -1,2 +1,10 @@
 -- Ejercicio 1
 DROP FUNCTION IF EXISTS sumar_dos_numeros;
+
+-- Ejercicio 2
+-- a)
+DROP FUNCTION IF EXISTS formatoStockSep2024();
+
+DELETE FROM stock
+WHERE EXTRACT(MONTH FROM fecha) = 9 AND EXTRACT(YEAR FROM fecha) = 2024;
+
