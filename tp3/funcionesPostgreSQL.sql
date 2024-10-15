1b3b8cce8621c8e9ceca034164f68d1349bb93fb
Dana Contreras <70418201+DanaContreras@users.noreply.github.com>
2024-10-15 11:48:06 -0300

    feat: ej2a

--- tp3/funcionesPostgreSQL.sql
+++ tp3/funcionesPostgreSQL.sql
@@ -7,3 +7,17 @@ RETURN a + b;
 END;
 $$
 LANGUAGE plpgsql;
+
+-- Ejercicio 2
+-- a)
+CREATE FUNCTION formatoStockSep2024 ()
+RETURNS TABLE(nombre VARCHAR, dosis INT, fecha DATE, cantidad INT)
+AS $$
+BEGIN
+	RETURN QUERY
+	SELECT m.nombre, m.dosis, s.fecha, s.cantidad
+	FROM medicamento m JOIN stock s ON m.idMedicamento = s.idMedicamento
+	WHERE EXTRACT(MONTH FROM s.fecha) = 9 AND EXTRACT(YEAR FROM s.fecha) = 2024;
+END;
+$$
+Language plpgsql
