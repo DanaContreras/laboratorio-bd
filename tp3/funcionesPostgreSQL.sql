05d8c58bab7cfda0aa4a8d132baa31fbbc5b7d9f
Dana Contreras <70418201+DanaContreras@users.noreply.github.com>
2024-10-16 23:22:04 -0300

    fix: ej2a formato de salida

--- tp3/funcionesPostgreSQL.sql
+++ tp3/funcionesPostgreSQL.sql
@@ -11,11 +11,15 @@ LANGUAGE plpgsql;
 -- Ejercicio 2
 -- a)
 CREATE FUNCTION formatoStockSep2024 ()
-RETURNS TABLE(nombre VARCHAR, dosis INT, fecha DATE, cantidad INT)
+RETURNS TABLE(formato TEXT)
 AS $$
 BEGIN
 	RETURN QUERY
-	SELECT m.nombre, m.dosis, s.fecha, s.cantidad
+	SELECT 'El medicamento ' || m.nombre ||
+	', con dosis ' || m.dosis ||
+	' registró un stock de ' || s.cantidad ||
+	' unidades, el ' || TO_CHAR(s.fecha, 'DD-MM-YYYY') ||
+	' a las ' || TO_CHAR(s.fecha, 'HH24:MI:SS') || '.'
 	FROM medicamento m JOIN stock s ON m.idMedicamento = s.idMedicamento
 	WHERE EXTRACT(MONTH FROM s.fecha) = 9 AND EXTRACT(YEAR FROM s.fecha) = 2024;
 END;
