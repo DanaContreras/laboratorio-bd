942c3f30d52f35a2291384fe2ddd1bb38d5033bd
Dana Contreras <70418201+DanaContreras@users.noreply.github.com>
2024-10-24 12:36:48 -0300

    feat:ej e triggers

--- tp3/mariaDB/delete.sql
+++ tp3/mariaDB/delete.sql
@@ -1,2 +1,8 @@
 
 
+-- Procedimientos y triggers:
+
+-- e)
+DROP TRIGGER IF EXISTS registrarLogEntregaMedicamento_insert;
+DROP TRIGGER IF EXISTS registrarLogEntregaMedicamento_delete;
+DROP TRIGGER IF EXISTS registrarLogEntregaMedicamento_update;
