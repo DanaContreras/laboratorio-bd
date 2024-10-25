8df34a8623c5018dd1999228f2497a909a5ed7bf
Dana Contreras <70418201+DanaContreras@users.noreply.github.com>
2024-10-25 18:13:59 -0300

    feat: triggers ej a

--- tp3/mariaDB/delete.sql
+++ tp3/mariaDB/delete.sql
@@ -9,6 +9,14 @@ DROP FUNCTION IF EXISTS sumar_dos_numeros;
 
 -- PROCEDIMIENTOS Y TRIGGERS:
 
+-- Ejercicio 2
+
+-- a)
+DROP FUNCTION IF EXISTS formatoStockSep2024;
+
+DELETE FROM stock
+WHERE MONTH(fecha) = 9 AND YEAR(fecha) = 2024;
+
 -- e)
 DROP TRIGGER IF EXISTS registrarLogEntregaMedicamento_insert;
 DROP TRIGGER IF EXISTS registrarLogEntregaMedicamento_delete;
