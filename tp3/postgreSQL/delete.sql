309635ada778a1dbb62ca5c9e0ccd73fd4cf2da4
Dana Contreras <70418201+DanaContreras@users.noreply.github.com>
2024-10-25 16:34:29 -0300

    feat: ej triggers inciso c

--- tp3/postgreSQL/delete.sql
+++ tp3/postgreSQL/delete.sql
@@ -1,3 +1,5 @@
+-- FUNCTIONS
+
 -- Ejercicio 1
 DROP FUNCTION IF EXISTS sumar_dos_numeros;
 
@@ -7,4 +9,17 @@ DROP FUNCTION IF EXISTS formatoStockSep2024;
 
 DELETE FROM stock
 WHERE EXTRACT(MONTH FROM fecha) = 9 AND EXTRACT(YEAR FROM fecha) = 2024;
+
+-------------------------------------------------------------------------------------------
+
+-- PROCEDIMIENTOS Y TRIGGERS
+
+-- Ejercicio c)
+-- los datos insertados en el ejercicio fueron borrados con el trigger.
+DROP TRIGGER IF EXISTS habilitarBorradoMedicamento ON medicamento;
+DROP FUNCTION IF EXISTS deleteRefMedicamento;
+
+
+
+
 
