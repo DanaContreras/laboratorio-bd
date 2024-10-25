eadc92ed478691f350e590a79fd33ba84c3ed090
Dana Contreras <70418201+DanaContreras@users.noreply.github.com>
2024-10-25 18:08:55 -0300

    feat: funciones ej 1 a

--- tp3/mariaDB/delete.sql
+++ tp3/mariaDB/delete.sql
@@ -1,8 +1,19 @@
+-- FUNCIONES
 
+-- Ejercicio 1
 
--- Procedimientos y triggers:
+-- a)
+DROP FUNCTION IF EXISTS sumar_dos_numeros;
+
+----------------------------------------------------------------------------------------
+
+-- PROCEDIMIENTOS Y TRIGGERS:
 
 -- e)
 DROP TRIGGER IF EXISTS registrarLogEntregaMedicamento_insert;
 DROP TRIGGER IF EXISTS registrarLogEntregaMedicamento_delete;
 DROP TRIGGER IF EXISTS registrarLogEntregaMedicamento_update;
+
+----------------------------------------------------------------------------------------
+
+-- CURSORES
