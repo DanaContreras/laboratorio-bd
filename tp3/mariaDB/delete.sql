81de78901a141ab37d226ff8143829e78e0c7d3c
Dr4g0nJ4r <50975740+Dr4g0nJ4r@users.noreply.github.com>
2024-11-22 02:35:09 -0300

    Update delete.sql

--- tp3/mariaDB/delete.sql
+++ tp3/mariaDB/delete.sql
@@ -8,7 +8,11 @@ DROP FUNCTION IF EXISTS sumar_dos_numeros;
 --b)
 DROP FUNCTION IF EXISTS multiplicar;
 
--- Ejercicio 2
+-- 1.2)
+-- b)
+DROP FUNCTION IF EXISTS diferenciaEnMeses;
+-- c)
+--d)
 --e)
 DROP FUNCTION IF EXISTS antiguedad_persona;
 
@@ -30,7 +34,16 @@ DROP FUNCTION IF EXISTS formatoStockSep2024;
 DELETE FROM stock
 WHERE MONTH(fecha) = 9 AND YEAR(fecha) = 2024;
 
+-- b)
+DROP TRIGGER IF EXISTS sumarCargo;
+
+DELETE FROM asignado WHERE fecha='10-23-2024' AND legajo='ADM010' AND nombreCargo='Director';
 -- c)
+
+update recetamedica 
+set fecha = '2024-08-30'
+where idreceta = 20315463
+
 -- los datos insertados en el ejercicio fueron borrados con el trigger.
 DROP TRIGGER IF EXISTS habilitarBorradoMedicamento;
 
@@ -43,6 +56,7 @@ DROP TRIGGER IF EXISTS registrarLogEntregaMedicamento_update;
 DROP TABLE IF EXISTS LOG_entregamedicamento;
 
 -- f)
+-- AFIRMACION 1
 DROP TRIGGER IF EXISTS restriccion_registro_turno_insert;
 DROP TRIGGER IF EXISTS restriccion_registro_turno_update;
 -- eliminacion de los datos de prueba utilizados para el inciso:
@@ -54,6 +68,11 @@ WHERE legajo IN ('ADM0011', 'PSC005');
 
 DELETE FROM personalClinico
 WHERE legajo = 'ADM0011';
+
+-- AFIRMACION 2
+DROP TRIGGER IF EXISTS restriccion_30_sot_create;
+DROP TRIGGER IF EXISTSrestriccion_30_sot_update;
+DELETE FROM sot WHERE legajoacompaniante = 'ACO123457';
 
 ----------------------------------------------------------------------------------------
 
